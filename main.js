a0e93e17f64bbf90183800a04175cfa20489c4ea
fabimmu <fabi.montoya20@gmail.com>
2021-07-31 16:15:29 -0600

    Maquetado final

---
+++ main.js
@@ -0,0 +1,13 @@
+const $ = (selector) => document.querySelector(selector);
+$("#btn-ocultar-filtros").addEventListener("click", () => {
+  $("#filtros-opciones").classList.add("is-hidden");
+})
+
+$("#btn-Nueva-Op").addEventListener("click", () => {
+  $("#section_nueva_operacion").classList.remove("is-hidden");
+  $("#section_inicio").classList.add("is-hidden");
+});
+$("#btn-new-op-cancelar").addEventListener("click", () => {
+  $("#section_nueva_operacion").classList.add("is-hidden");
+  $("#section_inicio").classList.remove("is-hidden");
+});
