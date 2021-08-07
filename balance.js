72d66579fc1dc30cb5bb874926d52556b5b9b4ff
fabimmu <fabi.montoya20@gmail.com>
2021-08-07 09:54:40 -0600

    Section Editar Categoria

--- balance.js
+++ balance.js
@@ -1,4 +1,13 @@
 const $ = (selector) => document.querySelector(selector);
+
+/*BURGER MENU OPTIONS*/
+const toggleBurger = () => {
+    let burgerIcon = document.getElementById('burger');
+    let dropMenu = document.getElementById('teamList');
+    burgerIcon.classList.toggle('is-active');
+    dropMenu.classList.toggle('is-active');
+  };
+
 $("#btn-ocultar-filtros").addEventListener("click", () => {
   $("#filtros-opciones").classList.add("is-hidden");
 });
@@ -11,3 +20,23 @@ $("#btn-new-op-cancelar").addEventListener("click", () => {
   $("#section_nueva_operacion").classList.add("is-hidden");
   $("#section_inicio").classList.remove("is-hidden");
 });
+
+/************************
+ * LOADING SELECT OPTIONS
+ ************************/
+const datos = getStorage();
+var loadSelectOptions = function () {
+  const { categorias } = getStorage();
+    var selectCategories = document.getElementById('selectCategories');
+  for (const category of categorias) {
+        var categoryOption = document.createElement('option');
+        categoryOption.innerText = category;
+        categoryOption.style.textTransform = "uppercase";
+        categoryOption.value = category;
+        selectCategories.appendChild(categoryOption);
+    }
+};
+    loadSelectOptions();
+
+
+
--- categorias.js
+++ categorias.js
@@ -5,16 +5,35 @@ const loadCategories = () => {
   const { categorias } = getStorage();
   let listCategories = document.getElementById("listCategories");
   for (const category of categorias) {
+    let boxCategory = document.createElement("div");
+    let elementBox = document.createElement("div");
     let element = document.createElement("li");
+    let BtnsBox = document.createElement("div");
+    let btnEdit = document.createElement("button");
+    let btnRemove = document.createElement("button");
+
+    boxCategory.setAttribute("class", "columns");
+    elementBox.setAttribute("class", "column list-item");
+    element.setAttribute("class", "tag is-primary is-light m-2 is-medium");
+    BtnsBox.setAttribute("class", "column in-line has-text-right");
+    btnEdit.setAttribute("class", "button is-warning m-2");
+        btnEdit.setAttribute("id", "btnEditCategory");
+    btnRemove.setAttribute("class", "button is-danger m-2");
+        btnRemove.setAttribute("id", "btnRemoveCategory");
+
     element.innerText = category;
     element.style.textTransform = "uppercase";
-    element.setAttribute("class", "tag is-primary is-light");
-    listCategories.appendChild(element);
+    btnEdit.innerText = "Editar";
+    btnRemove.innerText = "Eliminar";
+
+    listCategories.appendChild(boxCategory);
+    boxCategory.appendChild(elementBox);
+    boxCategory.appendChild(BtnsBox);
+    elementBox.appendChild(element);
+    BtnsBox.appendChild(btnEdit);
+    BtnsBox.appendChild(btnRemove);
   }
-
-  console.log(categorias);
 };
-
 loadCategories();
 
 const createCategories = () => {
@@ -25,7 +44,28 @@ const createCategories = () => {
   } else {
     datos.categorias.push(newCategoryName);
     localStorage.setItem("datos", JSON.stringify(datos));
+        $("#new-categoria-input").value = "";
   }
 };
-
 $("#new-categoria-btn").addEventListener("click", createCategories);
+
+/*EDITAR CATEGORIAS*/
+
+$("#btnEditCategory").addEventListener("click", () => {
+   $("#sectionEditCategory").classList.remove("is-hidden");
+  $("#sectionCategorias").classList.add("is-hidden");
+});
+$("#btnCancelEditCat").addEventListener("click", () => {
+   $("#sectionEditCategory").classList.add("is-hidden");
+  $("#sectionCategorias").classList.remove("is-hidden");
+});
+
+// const editCategory = (categoriaNueva, categorias) => {
+//   return categorias.map((categoria) =>
+//     categoriaNueva === categoria
+//       ? { ...categoria, ...categoriaNueva }
+//       : categoria
+//   )
+// }
+
+
